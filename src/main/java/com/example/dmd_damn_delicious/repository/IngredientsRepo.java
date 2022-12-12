401990cf7ec39bde82ef440e3e4473483c22426e
Damir Smintic <dsmintic@gmail.com>
2022-12-13 00:10:49 +0100

    Add User - Mode, Repository and Service (service and impl)

--- src/main/java/com/example/dmd_damn_delicious/repository/IngredientsRepo.java
+++ src/main/java/com/example/dmd_damn_delicious/repository/IngredientsRepo.java
@@ -1,4 +1,5 @@
 package com.example.dmd_damn_delicious.repository;
 
-public interface IngredientsRepo {
+public interface IngredientsRepository {
 }
+
--- src/main/java/com/example/dmd_damn_delicious/service/UserServiceImpl.java
+++ src/main/java/com/example/dmd_damn_delicious/service/UserServiceImpl.java
@@ -1,4 +1,80 @@
 package com.example.dmd_damn_delicious.service;
 
-public class UserServiceImpl {
+import com.example.dmd_damn_delicious.model.User;
+import com.example.dmd_damn_delicious.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.management.relation.RoleNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserByID(long id) {
+        Optional<User> optional = this.userRepository.findById(id);
+        User user = null;
+        if(optional.isPresent()){
+            user = optional.get();
+        }else{
+            throw new RuntimeException("User with id: " + id + " is not found");
+        }
+        return user;
+    }
+
+    @Override
+    public User saveUser(User user){
+        user.setPassword(user.getPassword());
+        return this.userRepository.save(user);
+    }
+
+//    @Override
+//    public User saveNewUser(User user) throws RoleNotFoundException {
+//        System.err.println("SaveNewUser: " + user);
+//
+//        user.setPassword(this.bcryptEncoder.encode(user.getPassword()));
+//
+//        Optional<Authority> optionalAuthority = this.authorityRepository.findByAuthority("ROLE_USER");
+//
+//        System.err.print("Optional Authority: " + optionalAuthority);
+//
+//        if (optionalAuthority.isPresent()) {
+//
+//            Authority authority = optionalAuthority.get();
+//            user.setAuthority(authority);
+//            System.err.println("blogUser after Roles: " + user);
+//
+//            return this.userRepository.saveAndFlush(user);
+//
+//        } else {
+//            throw new RoleNotFoundException("Default role not found for blog user with username " + user.getUsername());
+//        }
+//    }
+
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        Optional<User> user = userRepository.findByUsername(username);
+//        if (user.isPresent()) {
+//            return user.get();
+//        } else {
+//            throw new UsernameNotFoundException("No user found with username " + username);
+//        }
+//    }
 }
