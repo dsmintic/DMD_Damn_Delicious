f395ccb5ee8d533ed1986698aff2b75515a328d7
Damir Smintic <dsmintic@gmail.com>
2022-12-12 14:42:28 +0100

    adding few classes :)

---
+++ src/main/java/com/example/dmd_damn_delicious/controller/UserControllerRest.java
@@ -0,0 +1,11 @@
+package com.example.dmd_damn_delicious.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserControllerRest {
+
+    public UserControllerRest() {
+    }
+}
