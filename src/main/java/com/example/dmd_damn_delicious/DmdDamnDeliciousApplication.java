e461168c72b0b297e4c3d30c12205d06da2dcba0
Damir Smintic <dsmintic@gmail.com>
2022-12-12 10:10:59 +0100

    1st commit test

---
+++ src/main/java/com/example/dmd_damn_delicious/DmdDamnDeliciousApplication.java
@@ -0,0 +1,13 @@
+package com.example.dmd_damn_delicious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmdDamnDeliciousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmdDamnDeliciousApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dmd_damn_delicious/DmdDamnDeliciousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dmd_damn_delicious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmdDamnDeliciousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
