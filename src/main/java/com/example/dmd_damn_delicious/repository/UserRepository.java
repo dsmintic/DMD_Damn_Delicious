0213331802964a774fa36bcbd979a2163f3a85c2
Damir Smintic <dsmintic@gmail.com>
2022-12-13 00:13:03 +0100

    New UserRepository class

--- src/main/java/com/example/dmd_damn_delicious/repository/UserRepository.java
+++ src/main/java/com/example/dmd_damn_delicious/repository/UserRepository.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String userName);
-}
-
+    @Repository
+    public interface UserRepository extends JpaRepository<User, Long> {
+        Optional<User> findByUsername(String userName);
+    }
