18ac908a5a228dd3d3031a537b2ba36222ab130b
Damir Smintic <dsmintic@gmail.com>
2022-12-13 11:15:31 +0100

    Adding Rating classes

---
+++ src/main/java/com/example/dmd_damn_delicious/repository/RatingRepository.java
@@ -0,0 +1,14 @@
+package com.example.dmd_damn_delicious.repository;
+
+import com.example.dmd_damn_delicious.model.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    Optional<Rating> findById(long id);
+
+}
