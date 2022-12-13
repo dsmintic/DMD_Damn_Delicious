054709befa7baf52d54c880455a62928005c3ec8
Damir Smintic <dsmintic@gmail.com>
2022-12-13 09:29:10 +0100

    add Model - Comment

--- src/main/java/com/example/dmd_damn_delicious/model/Comment.java
+++ src/main/java/com/example/dmd_damn_delicious/model/Comment.java
@@ -1,4 +1,97 @@
 package com.example.dmd_damn_delicious.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table(name = "comments")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotEmpty(message = "Text cannot be empty")
+    @Column(name = "texts")
+    @NotNull
+    private String text;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @Column(name = "creation_date", nullable = false, updatable = false)
+    private Date creationDate;
+
+    @Temporal(TemporalType.TIMESTAMP) // pitati Denisa za update
+    @CreationTimestamp
+    @Column (name = "update_date", nullable = false, updatable = true)
+    private Date updateDate;
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Recipe recipe;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Comment() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", comment_text='" + text + '\'' +
+                ", creationDate=" + creationDate +
+                ", post_id=" + post.getId() + // can't go for post object itself as it will make a recursion and overflow a stack
+                ", username=" + user.getUsername() +
+                '}';
+    }
+
+
 }
--- src/main/java/com/example/dmd_damn_delicious/model/Recipe.java
+++ src/main/java/com/example/dmd_damn_delicious/model/Recipe.java
@@ -45,7 +45,7 @@ public class Recipe {
 
     @Temporal(TemporalType.TIMESTAMP) // pitati Denisa za update
     @CreationTimestamp
-    @Column
+   @Column (name = "update_date", nullable = false, updatable = true)
     private Date updateDate;
 
     @OneToMany(mappedBy = "recipes", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)//provjeriti da li je množina
