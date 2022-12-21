142bac45f0d91f03b67e6df4d59c4c8ce9e52404
Damir Smintic <dsmintic@gmail.com>
2022-12-21 23:07:48 +0100

    comment

---
+++ frontendVue/src/views/AddCommentView.vue
@@ -0,0 +1,14 @@
+<script>
+import CommentForm from '../components/CommentForm.vue';
+
+export default {
+    components: {
+        CommentForm
+    }
+}
+
+</script>
+
+<template>
+    <CommentForm></CommentForm>
+</template>
