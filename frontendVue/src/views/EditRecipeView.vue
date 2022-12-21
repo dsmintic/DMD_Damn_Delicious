ad0d37acf601e76698e3826daf46e455cf43eb4d
Damir Smintic <dsmintic@gmail.com>
2022-12-21 23:50:44 +0100

    edit comment components

---
+++ frontendVue/src/views/EditRecipeView.vue
@@ -0,0 +1,14 @@
+<script>
+import RecipeEdit from '../components/RecipeEdit.vue';
+
+export default {
+    components: {
+        RecipeEdit
+    }
+}
+
+</script>
+
+<template>
+    <RecipeEdit></RecipeEdit>
+</template>
