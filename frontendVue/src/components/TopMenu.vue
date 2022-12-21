cc800a9fe45d49abccbe774695a711d0d11cbbe4
Damir Smintic <dsmintic@gmail.com>
2022-12-21 15:43:59 +0100

    nav auth

--- frontendVue/src/components/TopMenu.vue
+++ frontendVue/src/components/TopMenu.vue
@@ -6,15 +6,15 @@ import Logout from "@/components/Logout.vue";
 
 export default defineComponent({
     components: { Logout },
-  methods: {
-    logout() {
-      this.authStore.logout()
-          .then(() => this.$router.push({ name: 'Home' }))
+    methods: {
+        logout() {
+            this.authStore.logout()
+                .then(() => this.$router.push({ name: 'Home' }))
+        },
     },
-  },
-  computed: {
-    ...mapStores(useAuthStore)
-  }
+    computed: {
+        ...mapStores(useAuthStore)
+    }
 });
 </script>
 
@@ -23,18 +23,18 @@ export default defineComponent({
     <section class="container">
 
         <div>
-            <router-link :to="{name:'Home'}"><img src="../images/dmd_logo.png" class="logoimage"></router-link>
+            <router-link :to="{ name: 'Home' }"><img src="../images/dmd_logo.png" class="logoimage"></router-link>
         </div>
 
         <div>
-            <router-link :to="{name:'Recipes'}">Recipes</router-link>
+            <router-link :to="{ name: 'Recipes' }">Recipes</router-link>
             <router-link to="#">Cook Calculator</router-link>
             <router-link :to="{ name: 'Video' }">Videos</router-link>
             <router-link :to="{ name: 'Chefs' }">Chefs</router-link>
-
         </div>
 
         <div>
+            <router-link :to="{ name: 'AddRecipe' }" v-if="authStore.isAuthenticated">Add recipe</router-link>
             <label>{{ authStore.username }}</label>
             <router-link :to="{ name: 'SignIn' }" v-if="!authStore.isAuthenticated">Sign in</router-link>
             <Logout></Logout>
