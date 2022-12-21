0d7ce56692b874a8533996ebfd1a097aeaad5ae9
Damir Smintic <dsmintic@gmail.com>
2022-12-21 11:37:11 +0100

    adding font

--- frontendVue/src/main.js
+++ frontendVue/src/main.js
@@ -6,7 +6,20 @@ import './assets/main.css'
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faTiktok } from '@fortawesome/free-brands-svg-icons'
+import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faUserSecret, faFacebook, faInstagram, faTiktok, faMapLocationDot, faPhone, faEnvelope )
+
 const app = createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
 const pinia = createPinia()
 
 pinia.use(piniaPluginPersistedstate)
